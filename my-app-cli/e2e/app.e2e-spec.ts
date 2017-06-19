06d720f5bc626104d3cb8402e887f60f60a09841
Yaroslav Putria <yaroslav.putria@globallogic.com>
2017-06-19 17:46:27 +0300

    cahged indentation

--- my-app-cli/e2e/app.e2e-spec.ts
+++ my-app-cli/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { MyAppCliPage } from './app.po';
 
 describe('my-app-cli App', () => {
-  let page: MyAppCliPage;
+    let page: MyAppCliPage;
 
-  beforeEach(() => {
-    page = new MyAppCliPage();
-  });
+    beforeEach(() => {
+        page = new MyAppCliPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!!');
-  });
+    it('should display welcome message', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('Welcome to app!!');
+    });
 });
