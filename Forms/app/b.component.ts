63a7aa0ea3219c48ae1cdb9e06c74895051ab5f0
Yaroslav Putria <yaroslav.putria@globallogic.com>
2017-07-11 19:10:04 +0300

    form array and onSubmit event

--- Forms/app/b.component.ts
+++ Forms/app/b.component.ts
@@ -1,6 +1,6 @@
 import { Component, AfterViewInit, ViewChild } from '@angular/core';
 
-import { FormControl, Validators, NgModel, FormGroup, NgForm } from '@angular/forms';
+import { FormControl, Validators, NgModel, FormGroup, FormArray, NgForm } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
@@ -8,9 +8,10 @@ import { FormControl, Validators, NgModel, FormGroup, NgForm } from '@angular/fo
     template: `
         <h4>B component is here!</h4>
         <h5>Reactive form</h5>
-        <form [formGroup]="myFormGroup">
+        <form [formGroup]="myFormGroup" (ngSubmit)="onSubmit(f)" #f="ngForm">
             <input type="text" formControlName="second"/> <!-- without input binding set as string -->
             <input type="text" [formControlName]="'first'"/> <!-- pass string in input binding -->
+            <br/><button type="submit">Submit</button>
         </form>
 
         <h5>Template driven form</h5>
@@ -18,6 +19,13 @@ import { FormControl, Validators, NgModel, FormGroup, NgForm } from '@angular/fo
             <input name="bla" ngModel/>
             <input name="ku" ngModel/>
         </form>
+
+        <h5>By FormArray</h5>
+        <ul [formGroup]="myFormArray">
+            <li *ngFor="let item of myFormArray.controls; let i = index">
+                <input type="text" formControlName="{{i}}">
+            </li>
+        </ul>
     `,
     styles: [`
         input {
@@ -33,7 +41,12 @@ export class BComponent implements AfterViewInit {
 
     @ViewChild(NgForm) secondFormGroup; // it took second form becouse of: "... selector: 'form:not([ngNoForm]):not([formGroup]),ngForm,[ngForm]', ..."
 
+    myFormArray = new FormArray([new FormControl(1), new FormControl(2)]);
+    onSubmit(ngFormGroupDirective) {
+        console.log(ngFormGroupDirective.form === this.myFormGroup);// true
+    }
     constructor() {
+        this.myFormGroup.valueChanges.subscribe(v => console.log('myFormGroup', v));
         console.log(this.myFormGroup.value); // {first: "first form control in form group", second: "second form control in form group"}
         setTimeout(() => {
             this.myFormGroup.setValue({first: 'new value1', second: 'new value22'});// updates whole form group, in there is no passed some control - throws error
@@ -43,10 +56,17 @@ export class BComponent implements AfterViewInit {
         }, 1000);
         setTimeout(() => {
             console.log(this.myFormGroup.status);// INVALID - 'second' control has validator 'maxLength'
+            this.myFormGroup.reset({first: 'new value1'});
+            console.log(this.myFormGroup.pristine);//true - reset method sets formGroup and its children pristine=true
         }, 2000);
+
+        setTimeout(() => {
+            this.myFormArray.setValue([5, 6]);
+        }, 4000);
     }
 
     ngAfterViewInit() {
+        this.secondFormGroup.valueChanges.subscribe(v => console.log('secondFormGroup', v));
         setTimeout(() => {
             this.secondFormGroup.setValue({bla: 'for bla', ku: 'for ku'});
         }, 3000);
