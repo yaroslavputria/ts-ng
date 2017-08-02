67a608f4dbbeec5d0bdec7b61a2b68c24c5905f7
Yaroslav Putria <yaroslav.putria@globallogic.com>
2017-08-02 12:58:35 +0300

    creating components on the fly

--- router/src/app/c.component.ts
+++ router/src/app/c.component.ts
@@ -1,6 +1,6 @@
 import { Component, ViewContainerRef, ViewChild, ChangeDetectorRef, Compiler, Injector } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { SystemJsNgModuleLoader, NgModuleFactoryLoader } from '@angular/core';
+import { SystemJsNgModuleLoader, NgModuleFactoryLoader, NgModuleRef, NgModule } from '@angular/core';
 
 import { LazyFormComponent } from './lazyModule/lazyform.component';
 
@@ -13,7 +13,8 @@ declare var System;
             <h2>I am CComponent with lazy loaded form component!</h2>
             <label><input [formControl]="checkBox" type="checkbox"/>Remove without saving</label><br />
             <button (click)="toggleLazyFrom()">Toggle lazy form</button>
-            <ng-container #lazyFormViewCont></ng-container>
+            <ng-container #lazyFormViewCont></ng-container><br />
+            <ng-container #superDynamic></ng-container>
             <!--<ng-container [ngTemplateOutlet]="tpl"></ng-container>
             <ng-container *ngComponentOutlet="ColorComponent"></ng-container>-->
         </div>
@@ -27,7 +28,7 @@ declare var System;
     ]
 })
 export class CComponent {
-    constructor(private jit: Compiler, private injector: Injector, private loader: NgModuleFactoryLoader) {
+    constructor(private jit: Compiler, private injector: Injector, private loader: NgModuleFactoryLoader, private _m: NgModuleRef<any>) {
     }
 
     checkBox = new FormControl(true);
@@ -85,6 +86,32 @@ export class CComponent {
                 this.vc.insert(this.view);
             }, 100);
         }
+    }
+
+    @ViewChild('superDynamic', {read: ViewContainerRef}) vcForSuperDynamic;
+
+    ngAfterViewInit() {
+        System.import('app/lazyModule/lazy.module.js').then((module) => {
+            this.jit.compileModuleAndAllComponentsAsync(module.LazyModule)
+            .then((compiled) => {
+                const m = compiled.ngModuleFactory.create(this.injector);
+                const factory = compiled.componentFactories[0];
+                const cmp = factory.create(this._injector, [], null, m);
+            })
+        })
+
+        ////--------------------------
+
+        const template = '<span class="bordered">generated on the fly: {{name}}</span>';
+        const tmpCmp = Component({template: template, styles: ['.bordered {border: 1px solid red}']})(class {});
+        const tmpModule = NgModule({declarations: [tmpCmp]})(class {});
+        this.jit.compileModuleAndAllComponentsAsync(tmpModule)
+            .then((factories) => {
+                const f = factories.componentFactories[0];
+                const cmpRef = f.create(this.injector, [], null, this._m);
+                cmpRef.instance.name = 'dynamic';
+                this.vcForSuperDynamic.insert(cmpRef.hostView);
+            })
     }
 
 }
